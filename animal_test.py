d1d877944264275ef01246068e09894405e4ad3b
Janne <janne@janliu.se>
2017-12-03 12:35:40 +0100

    New example

--- animal_test.py
+++ animal_test.py
@@ -5,21 +5,29 @@ class Sheep(Animal):
     def _walk(self, x, y):
         return self._random_walk(x, y)
 
+class Wolves(Animal):
+    def _walk(self, x, y):
+        return self._random_walk(x, y)
+
 
 def main():
     nLatticeLength = 256
+    Wolves.initialize(nLatticeLength)
     Sheep.initialize(nLatticeLength)
-    [Sheep() for i in range(1000)]
+    [Sheep() for i in range(10)]
+    [Wolves() for i in range(5)]
     plt.ioff()
     plt.show()
     plt.axis([-1, nLatticeLength, -1, nLatticeLength])
-    handle, = plt.plot([], [],  'o')
+    handleSheep, = plt.plot([], [],  'or')
+    handleWolves, = plt.plot([], [],  'ob')
     for i in range(1000):
+        Sheep.update_handler(handleSheep)
         Sheep.iterate()
-        Sheep.update_handler(handle)
+        Wolves.iterate()
+        Wolves.update_handler(handleWolves)
         plt.draw()
         plt.pause(0.02)
 
 if __name__ == "__main__":
     main()
-

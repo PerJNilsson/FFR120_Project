eb8967eeba323d7e827d1364df880c149290b3aa
Janne <janne@janliu.se>
2017-12-03 17:30:33 +0100

    Small fix

--- predator.py
+++ predator.py
@@ -4,18 +4,21 @@ import numpy as np
 
 
 class Predator(Animal):
+    hungerBeforeChase = 150
     def __init__(self, x=None, y=None, visibilityRadius=20, child=False):
         super().__init__(x, y, visibilityRadius, child=child)
         self.iterationsMovingToFood = 0
         self.previousStep = np.random.randint(1, 4, 1)
 
     def _eat(self, x, y):
+        if self.hunger > hungerBeforeChase:
+            return
         if Prey.grid[y][x]:
             Prey.grid[y][x].pop()
             self.hunger = Predator.maxHunger
 
     def _walk(self, x, y):
-        if self.hunger > 150:
+        if self.hunger > hungerBeforeChase:
             return (x, y)
         targetToChase = self._follow(x, y, Prey.grid)
         if targetToChase:
